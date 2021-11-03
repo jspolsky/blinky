f3acf3cd7810f7bbc846ee9bd47b70ee12c05972
Joel Spolsky <spolsky@fogcreek.com>
2021-11-03 11:36:22 -0400

    cleanup from last commit

--- blinky/src/button.h
+++ blinky/src/button.h
@@ -9,8 +9,4 @@ namespace Button
     void loop();
     void sleep();
     bool can_sleep();
-
-    void fnSwitch();    // called to switch display
-    void fnStartSwap(); // called to start swap with friend
-    void fnEndSwap();   // calle to end swap
 }
