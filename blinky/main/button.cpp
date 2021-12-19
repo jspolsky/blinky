33533eff9d17cfb61adbdb0d22a20061fe4fe7bd
Joel Spolsky <joel@spolsky.com>
2021-12-19 13:39:56 -0500

    short and long press logic

--- blinky/main/button.cpp
+++ blinky/main/button.cpp
@@ -3,7 +3,6 @@
 //
 // logic for the button.
 //
-// very short click - nothing. This is just to debounce.
 // short click - switch animation (on button up)
 // long click - start animation exchange.
 //
@@ -24,19 +23,34 @@ namespace button
 {
     void wakeup()
     {
+        uint64_t tmStart = esp_timer_get_time();
+        bool fInLongPress = false;
+
         gpio_set_direction(PIN_BUTTON, gpio_mode_t(GPIO_MODE_DEF_INPUT));
         gpio_set_pull_mode(PIN_BUTTON, GPIO_PULLDOWN_ONLY);
 
-        ESP_LOGI(TAG, "Looking at the button");
         while (gpio_get_level(PIN_BUTTON))
         {
-            ESP_LOGI(TAG, "Button is still down");
+            if (!fInLongPress &&
+                esp_timer_get_time() > (tmStart + pdMS_TO_TICKS(1000)))
+            {
+                ESP_LOGI(TAG, "Start Long Press");
+                fInLongPress = true;
+            }
+
+            // this kinda helps reduce bounces:
             vTaskDelay(pdMS_TO_TICKS(100));
         }
-        ESP_LOGI(TAG, "Button just went up!");
 
-        ESP_LOGI(TAG, "LED Rainbow Chase Start");
-        rgbled::setup();
-        rgbled::show_rainbow();
+        if (fInLongPress)
+        {
+            ESP_LOGI(TAG, "End Long Press");
+        }
+        else
+        {
+            ESP_LOGI(TAG, "Short Press");
+            rgbled::setup();
+            rgbled::show_rainbow();
+        }
     }
 }
