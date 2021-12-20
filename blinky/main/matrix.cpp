56508b85632a0a1a641afea7757d67bc7ba1544d
Joel Spolsky <joel@spolsky.com>
2021-12-20 11:18:49 -0500

    more programming by wishful thinking

--- blinky/main/matrix.cpp
+++ blinky/main/matrix.cpp
@@ -18,10 +18,27 @@ namespace matrix
     void setup()
     {
         ESP_LOGI(TAG, "Setting up matrix");
+
+        // TODO set up the power enable pin and make sure it stays in "on" mode
+        // whatever that is, and survives deep sleep
+
+        // TODO configure i2c (i2c_param_config)
+        // TODO install i2c driver (i2c_driver_install)
+
+        // TODO all of the things from Adafruit_IS31FL3731::begin, including
+        // clear()
     }
 
     void displayAnimation()
     {
+        // TODO this is basically the code from old matrix.cpp::displayAnimation
+
         ESP_LOGI(TAG, "Display initial animation");
+
+        // TODO get animation pixels, number of frames, and speed
+
+        // TODO load all the frames
+
+        // TODO start autoplay
     }
 }
