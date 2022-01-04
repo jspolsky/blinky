49eb97a6fe3e074708a1fc4620c24de88eb0d085
Joel Spolsky <joel@spolsky.com>
2022-01-04 16:00:14 -0500

    initial errors sending to serial -> fixed!

--- parsepng/parsepng.js
+++ parsepng/parsepng.js
@@ -81,160 +81,191 @@ while (ixArg < args.length) {
   ixArg++;
 }
 
-getPixels(inputFileName, function (err, pixels) {
-  if (err) {
-    console.error(`input file ${inputFileName} not found or not a PNG file`);
-    return;
-  }
+const main = () => {
+  getPixels(inputFileName, function (err, pixels) {
+    if (err) {
+      console.error(`input file ${inputFileName} not found or not a PNG file`);
+      return;
+    }
 
-  if (pixels.shape[0] < 9 || pixels.shape[0] > 72 || pixels.shape[0] % 9 != 0) {
-    console.error(
-      "Image must be a multiple of 9 pixels wide, no more than 72 pixels"
-    );
-    return;
-  }
+    if (
+      pixels.shape[0] < 9 ||
+      pixels.shape[0] > 72 ||
+      pixels.shape[0] % 9 != 0
+    ) {
+      console.error(
+        "Image must be a multiple of 9 pixels wide, no more than 72 pixels"
+      );
+      return;
+    }
 
-  const number_of_frames = pixels.shape[0] / 9;
+    const number_of_frames = pixels.shape[0] / 9;
 
-  if (pixels.shape[1] !== 16) {
-    console.error("Image must be 16 pixels high");
-    return;
-  }
+    if (pixels.shape[1] !== 16) {
+      console.error("Image must be 16 pixels high");
+      return;
+    }
 
-  const gamma_scale = [0, 1, 2, 3, 4, 5, 6, 8, 10, 12, 15, 19, 24, 30, 37, 47];
+    const gamma_scale = [
+      0, 1, 2, 3, 4, 5, 6, 8, 10, 12, 15, 19, 24, 30, 37, 47,
+    ];
 
-  let luminositySum = 0;
-  let luminosityCount = 0;
+    let luminositySum = 0;
+    let luminosityCount = 0;
 
-  const getOneByte = (x, y) => {
-    const red = pixels.get(x, y, 0);
-    const green = pixels.get(x, y, 0);
-    const blue = pixels.get(x, y, 0);
+    const getOneByte = (x, y) => {
+      const red = pixels.get(x, y, 0);
+      const green = pixels.get(x, y, 0);
+      const blue = pixels.get(x, y, 0);
 
-    // luminosity formula, in case the incoming image isn't greyscale already
-    const grey = Math.round(0.2126 * red + 0.7152 * green + 0.0722 * blue);
-    if (grey > 255) grey = 255;
+      // luminosity formula, in case the incoming image isn't greyscale already
+      const grey = Math.round(0.2126 * red + 0.7152 * green + 0.0722 * blue);
+      if (grey > 255) grey = 255;
 
-    const shade = grey >> 4;
-    luminositySum += gamma_scale[shade];
-    luminosityCount++;
+      const shade = grey >> 4;
+      luminositySum += gamma_scale[shade];
+      luminosityCount++;
 
-    return shade;
-  };
+      return shade;
+    };
 
-  const formatTwoNibbles = (nib1, nib2) => {
-    if (!nib1 && !nib2) return "____, ";
-    else return `0x${nib1.toString(16)}${nib2.toString(16)}, `;
-  };
+    const formatTwoNibbles = (nib1, nib2) => {
+      if (!nib1 && !nib2) return "____, ";
+      else return `0x${nib1.toString(16)}${nib2.toString(16)}, `;
+    };
 
-  var imageAsCCode = `const uint8_t bmp_${variableName}[] = \{
+    var imageAsCCode = `const uint8_t bmp_${variableName}[] = \{
     /* frames, delay (hi byte), delay (lo byte): */
     ${number_of_frames}, ${delay >> 8}, ${delay & 0xff},
 
     /* raw data: */\n`;
 
-  var imageAsByteArray = [
-    // first byte: number of frames
-    number_of_frames,
+    var imageAsByteArray = [
+      // first byte: number of frames
+      number_of_frames,
 
-    // second byte: hi order, delay in ms
-    parseInt(delay / 256),
+      // second byte: hi order, delay in ms
+      parseInt(delay / 256),
 
-    // third byte: lo order, delay in ms
-    delay % 256,
-  ];
+      // third byte: lo order, delay in ms
+      delay % 256,
+    ];
 
-  console.log(imageAsByteArray);
+    console.log(imageAsByteArray);
 
-  for (let frame = 0; frame < number_of_frames; frame++) {
-    for (let x = 0; x <= 8; x++) {
-      let row = "";
-      for (let y = 0; y <= 15; y += 2) {
-        const byte1 = getOneByte(frame * 9 + x, 15 - y); // 15 - because board is upside down
-        const byte2 = getOneByte(frame * 9 + x, 14 - y); // 14 - because board is upside down
+    for (let frame = 0; frame < number_of_frames; frame++) {
+      for (let x = 0; x <= 8; x++) {
+        let row = "";
+        for (let y = 0; y <= 15; y += 2) {
+          const byte1 = getOneByte(frame * 9 + x, 15 - y); // 15 - because board is upside down
+          const byte2 = getOneByte(frame * 9 + x, 14 - y); // 14 - because board is upside down
 
-        row += formatTwoNibbles(byte1, byte2);
+          row += formatTwoNibbles(byte1, byte2);
 
-        imageAsByteArray.push(byte1 * 16 + byte2);
+          imageAsByteArray.push(byte1 * 16 + byte2);
+        }
+        imageAsCCode += `\t${row}\n`;
       }
-      imageAsCCode += `\t${row}\n`;
+      imageAsCCode += "\n";
     }
-    imageAsCCode += "\n";
-  }
 
-  imageAsCCode += "};\n";
+    imageAsCCode += "};\n";
 
-  if (outputFileName !== "") {
-    //
-    // Write animation as binary to file
-    //
+    if (outputFileName !== "") {
+      //
+      // Write animation as binary to file
+      //
 
-    fs.writeFile(outputFileNameBinary, Buffer.from(imageAsByteArray), (err) => {
-      if (err) {
-        console.error(`Error writing output file ${outputFileNameBinary}`);
-        console.error(err);
-        return;
-      }
-    });
-
-    fs.writeFile(outputFileNameCCode, imageAsCCode, (err) => {
-      if (err) {
-        console.error(`Error writing output file ${outputFileNameCCode}`);
-        console.error(err);
-        return;
-      }
-    });
-  } else {
-    //
-    // Send output to USB port for preview
-    //
-
-    // find the path. If there is not exactly one Adafruit USB port,
-    // give up
-
-    var cPorts = 0;
-    var sPath = "";
-
-    SerialPort.list().then(
-      (ports) => {
-        ports.forEach((port) => {
-          if (port.manufacturer && port.manufacturer.startsWith("Adafruit")) {
-            sPath = port.path;
-            cPorts++;
-          }
-        });
-        if (cPorts > 1) {
-          console.error(
-            "More than one USB device found ... not sure who to talk to"
-          );
-          return;
-        }
-
-        if (cPorts < 1) {
-          console.error("Can't find Blinky Previewer connected to USB");
-          return;
-        }
-
-        // If we made it this far, send the data to the previewer
-        const port = new SerialPort(sPath, function (err) {
+      fs.writeFile(
+        outputFileNameBinary,
+        Buffer.from(imageAsByteArray),
+        (err) => {
           if (err) {
-            console.error("Open port error ", err.message);
+            console.error(`Error writing output file ${outputFileNameBinary}`);
+            console.error(err);
             return;
           }
-        });
+        }
+      );
 
-        port.write("!"); // starts transmission
-        port.write(imageAsByteArray);
-      },
-      (err) => console.error(err)
+      fs.writeFile(outputFileNameCCode, imageAsCCode, (err) => {
+        if (err) {
+          console.error(`Error writing output file ${outputFileNameCCode}`);
+          console.error(err);
+          return;
+        }
+      });
+    } else {
+      //
+      // Send output to USB port for preview
+      //
+
+      // find the path. If there is not exactly one Adafruit USB port,
+      // give up
+
+      var cPorts = 0;
+      var sPath = "";
+
+      SerialPort.list().then(
+        (ports) => {
+          ports.forEach((port) => {
+            if (port.manufacturer && port.manufacturer.startsWith("Adafruit")) {
+              sPath = port.path;
+              cPorts++;
+            }
+          });
+          if (cPorts > 1) {
+            console.error(
+              "More than one USB device found ... not sure who to talk to"
+            );
+            return;
+          }
+
+          if (cPorts < 1) {
+            console.error("Can't find Blinky Previewer connected to USB");
+            return;
+          }
+
+          // If we made it this far, send the data to the previewer
+          const port = new SerialPort(sPath, function (err) {
+            if (err) {
+              console.error("Open port error ", err.message);
+              return;
+            }
+          });
+
+          port.flush(() => {
+            console.log("flushed");
+            setTimeout(() => {
+              port.write("!"); // starts transmission
+              port.write(imageAsByteArray);
+              port.drain(() => {
+                console.log("drained");
+                setTimeout(() => {
+                  port.close();
+                  console.log("closed");
+                }, 1000);
+              });
+            }, 100);
+          });
+        },
+        (err) => console.error(err)
+      );
+    }
+
+    console.log(
+      `Average luminosity: ${
+        luminositySum / luminosityCount
+      }\nEst mA consumption: ${
+        2.5 + 0.60851064 * (luminositySum / luminosityCount)
+      } `
     );
-  }
+  });
+};
 
-  console.log(
-    `Average luminosity: ${
-      luminositySum / luminosityCount
-    }\nEst mA consumption: ${
-      2.5 + 0.60851064 * (luminositySum / luminosityCount)
-    } `
-  );
-});
+if (watch) {
+  main(); // run the first time
+  setInterval(main, 5000); // and every 5 seconds
+} else {
+  main();
+}
